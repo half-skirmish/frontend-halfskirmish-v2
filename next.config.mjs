17c5184592d03fea9633b43a1af73dce42d79edb
naman-1905 <namanoj19@gmail.com>
2025-08-23 20:03:50 +0530

    Updated the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbo: false, 
+    turbo: false,
   },
-  output: "standalone"
+  output: "standalone",
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
