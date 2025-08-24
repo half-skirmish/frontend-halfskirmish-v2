f1e6c6721ddcb4419522f4f4f9ac4bb17d68d1fb
naman-1905 <namanoj19@gmail.com>
2025-08-24 12:42:32 +0530

    Update the Footer for the responsive view

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,10 +1,11 @@
 const Footer = () => {
     return (
-      <footer className="bg-gray-900 text-center text-white py-8 z-30">
-        <p>&copy; {new Date().getFullYear()} Half Skirmish. All rights reserved.</p>
+      <footer className="bg-gray-900 text-center text-white py-8 z-30 w-full max-w-[100vw] overflow-x-hidden">
+        <div className="container mx-auto px-4">
+          <p>&copy; {new Date().getFullYear()} Half Skirmish. All rights reserved.</p>
+        </div>
       </footer>
     );
   };
   
   export default Footer;
-  
