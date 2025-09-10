0db77b7a308171e74b70c90d6171ea62a096f033
naman-1905 <namanoj19@gmail.com>
2025-09-10 13:04:52 +0530

    Added support for images from unsplash

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,25 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Add other domains as needed
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
